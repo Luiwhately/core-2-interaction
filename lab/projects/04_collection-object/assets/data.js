9ad6ef71c5e50159844360074ff9fedcd95e03d3
Luiwhately <whatl947@newschool.edu>
2024-03-21 21:29:14 -0400

    updating

---
+++ lab/projects/04_collection-object/assets/data.js
@@ -0,0 +1,24 @@
+// An array of objects representing my collection:
+var run = [
+"100px-runOne-SunnySunday"
+    {
+        miles: '1to3',
+        startingLocation: 'GramarcyPark',
+       review: '4/5',
+       Looped: 'yes'
+
+    },
+"100px-RunTwo-HudsonRiver"
+    {
+        miles: '4to6',
+        startingLocation: 'Chelsea',
+       review: '5/5',
+       Looped: 'yes'
+    },
+    {
+        miles: '1to3',
+        startingLocation: 'GramarcyPark',
+       review: '4/5',
+       Looped: 'yes'
+    },
+];
