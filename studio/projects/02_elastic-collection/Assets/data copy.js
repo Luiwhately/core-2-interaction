90714492da15f48d69cd0c29f6c0755dbf1fc70e
Luiwhately <whatl947@newschool.edu>
2024-03-28 22:29:07 -0400

    a lot

---
+++ studio/projects/02_elastic-collection/Assets/data copy.js
@@ -0,0 +1,132 @@
+// An array of objects representing my collection:
+var tracks = [
+    {
+        startingLocation: 'Gramercy Park',
+        image: '144E24thSttoRockefellerPark.png',
+        miles: 5,
+        review: 5,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Gramercy Park',
+        image: '144E24thSttoRockefellerParkLoop.png',
+        miles: 6,
+        review: 4,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Gramercy Park',
+        image: '144E24thtoCentralParkLoop.png',
+        miles: 10.3,
+        review: 3,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Brooklyn',
+        image: 'BigLoopAtProspectPark.png',
+        miles: 3.6,
+        review: 5,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Chelsea',
+        image: 'LittleIslandtoOneWorldTrade.png',
+        miles: 4.2,
+        review: 4,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Gramercy Park',
+        image: 'MadisonSquareParktoEastRiverParkField8.png',
+        miles: 4.8,
+        review: 3,
+        looped: false,
+        
+    },
+    {
+        startingLocation: ['Upper West', 'Upper East'],
+        image: 'OneLoopatCentralPark.png',
+        miles: 4.4,
+        review: 4,
+        looped: true,
+        
+    },
+    {
+        startingLocation: ['Upper West', 'Upper East'],
+        image: 'OneLoopatShumanRunningTrack.png',
+        miles: 1.6,
+        review: 5,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Brooklyn',
+        image: 'PebbleBeachtoBrooklynBridgeParkLoop.png',
+        miles: 2.2,
+        review: 4,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Upper West',
+        image: 'RiversideParkFromEmpireStateTrailtoColumbiaUniversity.png',
+        miles: 7.8,
+        review: 4,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Roosevelt Island',
+        image: 'RooseveltIsland.png',
+        miles: 4.5,
+        review: 5,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Brooklyn',
+        image: 'SmallLoopAtProspectPark.png',
+        miles: 2.2,
+        review: 5,
+        looped: true,
+        
+    },
+    {
+        startingLocation: 'Lower East',
+        image: 'StuyvesantSquareParktoWilliansburgBridgeLoop.png',
+        miles: 5.4,
+        review: 3,
+        looped: true,
+        
+    },
+    {
+        startingLocation: ['Upper West', 'Uper East'],
+        image: 'ThreeLoopsatShumanRunningTrack.png',
+        miles: 4.8,
+        review: 3,
+        looped: true,
+        
+    },
+    {
+        startingLocation: ['Upper West', 'Uper East'],
+        image: 'ThreeLoopsInCentralPark.png',
+        miles: 13,
+        review: 4,
+        looped: true,
+        
+    },
+    {
+        startingLocation: ['Chelsea'],
+        image: 'VesseltoHudsonRiverGreenwayLoop.png',
+        miles: 3.8,
+        review: 3,
+        looped: true,
+        
+    },
+
+];
